70e70fe5ae305ae8a0c28af2050c327d54ce8ea8
AidanQB1 <92817224+AidanQB1@users.noreply.github.com>
2021-11-01 13:38:08 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . # # #
+        . . . # .
+        . . # . #
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . . . #
+        . . . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("Watch Out Children At Play!")
+})
